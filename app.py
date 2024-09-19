a92ac08aaa701af9a2611936739f0d8aa9d6a323
Aitor <aitorgaillard99@gmail.com>
2024-09-19 18:44:28 +0200

    show submission notification

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+# create the flask app with special keys and configuration
 app = Flask(__name__)
-app.config["SECRET-KEY"] = "myapp123"
+app.config["SECRET_KEY"] = "myapp123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+# create the database related to the app with SQLAlchemy
 db = SQLAlchemy(app)
 
 # create database model
@@ -28,12 +31,14 @@ def index():
         date_object = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
 
+        # create the db.Model
         form = Form(first_name=first_name, last_name=last_name, email=email,
                     date=date_object, occupation=occupation)
 
         # add data from the form to the database
         db.session.add(form)
         db.session.commit()
+        flash(f"Form submitted {first_name}", "success")
 
     return render_template("index.html")
 
@@ -41,4 +46,5 @@ if __name__ == "__main__":
     # this will make the database if it does not exist
     with app.app_context():
         db.create_all()
-app.run(debug=True, port=5001)
+    # runs the app
+    app.run(debug=True, port=5001)
